6bcc639828d0eb2fa5dda53d297d654a33b48893
Sam Stenvall <neggelandia@gmail.com>
2014-11-04 12:02:39 +0200

    initial import

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 node-ffmpeg-mpegts-proxy
 ========================
 
-Simple proxy for leveraging ffmpeg to convert any source URL into MPEG-TS over HTTP
+Simple proxy for leveraging ffmpeg to convert any source URL into MPEG-TS over HTTP. It has been designed for proxying HLS streams for use as IPTV input in tvheadend.
+
+## Requirements
+
+* nodejs
+
+## Usage
+
+* install the required libraries by running `npm install` in the project directory
+* modify `data/sources.json`
+* run the program using `nodejs node-ffmpeg-mpegts-proxy.js`
---
+++ node-ffmpeg-mpegts-proxy.js
@@ -0,0 +1,96 @@
+/*
+ * Read command line options
+ */
+var argv = require('yargs')
+		.usage('Usage: $0 -p <port> [-a <avconv>]')
+		.alias('p', 'port')
+		.alias('a', 'avconv')
+		.demand(['p'])
+		.default('a', 'avconv')
+		.describe('p', 'The port the HTTP server should be listening on')
+		.describe('a', 'The path to avconv, defaults to just "avconv"')
+		.argv;
+
+/*
+ * Configure logger
+ */
+var winston = require('winston');
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {
+	timestamp: true,
+	colorize: true,
+	level: 'debug'
+});
+
+/*
+ * Include other libraries
+ */
+var http = require("http");
+var spawn = require('child_process').spawn;
+var fs = require('fs');
+
+/*
+ * Read the source definitions
+ */
+var sources = JSON.parse(fs.readFileSync('data/sources.json', 'utf8'));
+
+/**
+ * The main HTTP server process
+ * @type @exp;http@call;createServer
+ */
+var server = http.createServer(function (request, response) {
+	winston.debug('Got request for "' + request.url + '" from ' + request.connection.remoteAddress);
+
+	// Determine which source to serve based on the requested URL
+	var source = null;
+
+	for (var i = 0; i < sources.length; i++)
+	{
+		if (sources[i].url === request.url)
+		{
+			source = sources[i];
+			break;
+		}
+	}
+
+	if (source === null)
+	{
+		winston.info('Unknown source "' + request.url + '" requested');
+
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.write("404 Not Found\n");
+		response.end();
+
+		return;
+	}
+
+	// Tell the client we're sending MPEG-TS data
+	response.writeHead(200, {'Content-Type': 'video/mp2t'});
+
+	// Define options for the child process
+	var avconvOptions = [
+		'-i', source.source,
+		'-vcodec', 'copy',
+		'-acodec', 'copy',
+		'-metadata', 'service_name="' + source.name + '"',
+		'-f', 'mpegts',
+		'-' // Use stdout as output
+	];
+
+	var avconv = spawn(argv.avconv, avconvOptions);
+
+	// Relay video output to the client
+	avconv.stdout.on('data', function (chunk) {
+		response.write(chunk);
+	});
+
+	// Kill avconv when client closes the connection
+	request.on('close', function () {
+		winston.info('Client disconnected, stopping avconv');
+		avconv.kill();
+	});
+});
+
+// Start the server
+server.listen(argv.port);
+winston.info('Server listening on port ' + argv.port);
